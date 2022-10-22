a2857a745c47c77544412caa415d59bb6ead05a5
Valglez <71898653+valglez@users.noreply.github.com>
2022-10-22 13:36:23 +0200

    Added POST method test (#2)

---
+++ code/test/conn.php
@@ -0,0 +1,12 @@
+<?php
+
+$databaseHost = 'db';
+$databaseUsername = 'root';
+$databasePassword = 'example';
+
+$conn = mysqli_connect($databaseHost, $databaseUsername, $databasePassword);
+
+if (!$conn) {
+  die('Could not connect to MariaDB!: ' . mysqli_connect_error());
+}
+echo 'Connected to MariaDB!';
